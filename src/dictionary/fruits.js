ce0841adc92a40dcb02947ce565f95843035dba7
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-06-09 15:10:59 +0200

    Update fruits.js data

--- src/dictionary/fruits.js
+++ src/dictionary/fruits.js
@@ -1,3 +1,75 @@
-const fruits = {
-    name: 'Apple'
-}
+const imgPath = '../../public/images/fruits'
+
+const fruits = [{
+    name: 'Apple',
+    img: `${imgPath}/apple.png`
+}, {
+    name: 'Banana',
+    img: `${imgPath}/banana.png`
+}, {
+    name: 'Blackberry',
+    img: `${imgPath}/blackberry.png`
+}, {
+    name: 'Blueberries',
+    img: `${imgPath}/blueberries.png`
+}, {
+    name: 'Cherries',
+    img: `${imgPath}/cherries.png`
+}, {
+    name: 'Coconut',
+    img: `${imgPath}/coconut.png`
+}, {
+    name: 'Dragon Fruit',
+    img: `${imgPath}/dragon_fruit.png`
+}, {
+    name: 'Fig',
+    img: `${imgPath}/fig.png`
+}, {
+    name: 'Grapes',
+    img: `${imgPath}/grapes.png`
+}, {
+    name: 'Kiwi',
+    img: `${imgPath}/kiwi.png`
+}, {
+    name: 'Lemon',
+    img: `${imgPath}/lemon.png`
+}, {
+    name: 'Lychee',
+    img: `${imgPath}/lychee.png`
+}, {
+    name: 'Mango',
+    img: `${imgPath}/mango.png`
+}, {
+    name: 'Melon',
+    img: `${imgPath}/melon.png`
+}, {
+    name: 'Orange',
+    img: `${imgPath}/orange.png`
+}, {
+    name: 'Papaya',
+    img: `${imgPath}/papaya.png`
+}, {
+    name: 'Peach',
+    img: `${imgPath}/peach.png`
+}, {
+    name: 'Pear',
+    img: `${imgPath}/pear.png`
+}, {
+    name: 'Pineapple',
+    img: `${imgPath}/pineapple.png`
+}, {
+    name: 'Plum',
+    img: `${imgPath}/plum.png`
+}, {
+    name: 'Pomegranate',
+    img: `${imgPath}/pomegranate.png`
+}, {
+    name: 'Star Fruit',
+    img: `${imgPath}/star_fruit.png`
+}, {
+    name: 'Strawberry',
+    img: `${imgPath}/strawberry.png`
+}, {
+    name: 'Watermelon',
+    img: `${imgPath}/watermelon.png`
+}]
