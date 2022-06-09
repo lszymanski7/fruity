28a236bf15fb506481cfcc20a75e0b02b8d8a719
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-06-09 18:28:04 +0200

    Rename some files and update index.js file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: {
-		main: path.resolve(__dirname, './src/app.js')
+		main: path.resolve(__dirname, './src/index.js')
 	},
   	output: {
 		path: path.resolve(__dirname, './public/dist'),
