505f4774bf253b83e5fd8ce9be7e639eb22c4cdd
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-06-17 01:28:25 +0200

    Update fruits.js file - add a new property (checked)

--- src/dictionary/fruits.js
+++ src/dictionary/fruits.js
@@ -5,145 +5,169 @@ const fruits = [{
     img: [
         fruitsColor['apple.png'].default,
         fruitsLinear['apple.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Banana',
     img: [
         fruitsColor['banana.png'].default,
         fruitsLinear['banana.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Blackberry',
     img: [
         fruitsColor['blackberry.png'].default,
         fruitsLinear['blackberry.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Blueberries',
     img: [
         fruitsColor['blueberries.png'].default,
         fruitsLinear['blueberries.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Cherries',
     img: [
         fruitsColor['cherries.png'].default,
         fruitsLinear['cherries.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Coconut',
     img: [
         fruitsColor['coconut.png'].default,
         fruitsLinear['coconut.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Dragon Fruit',
     img: [
         fruitsColor['dragon-fruit.png'].default,
         fruitsLinear['dragon-fruit.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Fig',
     img: [
         fruitsColor['fig.png'].default,
         fruitsLinear['fig.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Grapes',
     img: [
         fruitsColor['grapes.png'].default,
         fruitsLinear['grapes.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Kiwi',
     img: [
         fruitsColor['kiwi.png'].default,
         fruitsLinear['kiwi.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Lemon',
     img: [
         fruitsColor['lemon.png'].default,
         fruitsLinear['lemon.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Lychee',
     img: [
         fruitsColor['lychee.png'].default,
         fruitsLinear['lychee.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Mango',
     img: [
         fruitsColor['mango.png'].default,
         fruitsLinear['mango.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Melon',
     img: [
         fruitsColor['melon.png'].default,
         fruitsLinear['melon.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Orange',
     img: [
         fruitsColor['orange.png'].default,
         fruitsLinear['orange.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Papaya',
     img: [
         fruitsColor['papaya.png'].default,
         fruitsLinear['papaya.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Peach',
     img: [
         fruitsColor['peach.png'].default,
         fruitsLinear['peach.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Pear',
     img: [
         fruitsColor['pear.png'].default,
         fruitsLinear['pear.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Pineapple',
     img: [
         fruitsColor['pineapple.png'].default,
         fruitsLinear['pineapple.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Plum',
     img: [
         fruitsColor['plum.png'].default,
         fruitsLinear['plum.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Pomegranate',
     img: [
         fruitsColor['pomegranate.png'].default,
         fruitsLinear['pomegranate.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Star Fruit',
     img: [
         fruitsColor['star-fruit.png'].default,
         fruitsLinear['star-fruit.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Strawberry',
     img: [
         fruitsColor['strawberry.png'].default,
         fruitsLinear['strawberry.png'].default
-    ]
+    ],
+    checked: true
 }, {
     name: 'Watermelon',
     img: [
         fruitsColor['watermelon.png'].default,
         fruitsLinear['watermelon.png'].default
-    ]
+    ],
+    checked: true
 }]
 
 export { fruits as default }
