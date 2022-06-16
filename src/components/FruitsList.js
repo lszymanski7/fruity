676326b9bbf7bb1c7f5d82cca2f678f3a39e942d
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-06-16 16:51:38 +0200

    Update fruits.js file - change img property

--- src/components/FruitsList.js
+++ src/components/FruitsList.js
@@ -1,19 +1,12 @@
 import React from 'react'
-import FruitsItem from './FruitsItem'
 import fruits from '../dictionary/fruits'
 
 const FruitsList = () => {
-    console.log(fruits)
 
     return (
-        // <div className="container__list">
-        //     <FruitsItem />
-        // </div>
-
         <div>
 		    <div className="widget-header">
 			    <h3 className="widget-header__title">Your Fruits</h3>
-                <img src={fruits[0].img[0].default} />
             </div>
 		</div>
     ) 
--- src/dictionary/fruits.js
+++ src/dictionary/fruits.js
@@ -3,155 +3,147 @@ import { fruitsColor, fruitsLinear } from '../assets/icons/index'
 const fruits = [{
     name: 'Apple',
     img: [
-        fruitsColor['apple.png'], 
-        fruitsLinear['apple.png']
+        fruitsColor['apple.png'].default,
+        fruitsLinear['apple.png'].default
+    ]
+}, {
+    name: 'Banana',
+    img: [
+        fruitsColor['banana.png'].default,
+        fruitsLinear['banana.png'].default
+    ]
+}, {
+    name: 'Blackberry',
+    img: [
+        fruitsColor['blackberry.png'].default,
+        fruitsLinear['blackberry.png'].default
+    ]
+}, {
+    name: 'Blueberries',
+    img: [
+        fruitsColor['blueberries.png'].default,
+        fruitsLinear['blueberries.png'].default
+    ]
+}, {
+    name: 'Cherries',
+    img: [
+        fruitsColor['cherries.png'].default,
+        fruitsLinear['cherries.png'].default
+    ]
+}, {
+    name: 'Coconut',
+    img: [
+        fruitsColor['coconut.png'].default,
+        fruitsLinear['coconut.png'].default
+    ]
+}, {
+    name: 'Dragon Fruit',
+    img: [
+        fruitsColor['dragon-fruit.png'].default,
+        fruitsLinear['dragon-fruit.png'].default
+    ]
+}, {
+    name: 'Fig',
+    img: [
+        fruitsColor['fig.png'].default,
+        fruitsLinear['fig.png'].default
+    ]
+}, {
+    name: 'Grapes',
+    img: [
+        fruitsColor['grapes.png'].default,
+        fruitsLinear['grapes.png'].default
+    ]
+}, {
+    name: 'Kiwi',
+    img: [
+        fruitsColor['kiwi.png'].default,
+        fruitsLinear['kiwi.png'].default
+    ]
+}, {
+    name: 'Lemon',
+    img: [
+        fruitsColor['lemon.png'].default,
+        fruitsLinear['lemon.png'].default
+    ]
+}, {
+    name: 'Lychee',
+    img: [
+        fruitsColor['lychee.png'].default,
+        fruitsLinear['lychee.png'].default
+    ]
+}, {
+    name: 'Mango',
+    img: [
+        fruitsColor['mango.png'].default,
+        fruitsLinear['mango.png'].default
+    ]
+}, {
+    name: 'Melon',
+    img: [
+        fruitsColor['melon.png'].default,
+        fruitsLinear['melon.png'].default
+    ]
+}, {
+    name: 'Orange',
+    img: [
+        fruitsColor['orange.png'].default,
+        fruitsLinear['orange.png'].default
+    ]
+}, {
+    name: 'Papaya',
+    img: [
+        fruitsColor['papaya.png'].default,
+        fruitsLinear['papaya.png'].default
+    ]
+}, {
+    name: 'Peach',
+    img: [
+        fruitsColor['peach.png'].default,
+        fruitsLinear['peach.png'].default
+    ]
+}, {
+    name: 'Pear',
+    img: [
+        fruitsColor['pear.png'].default,
+        fruitsLinear['pear.png'].default
+    ]
+}, {
+    name: 'Pineapple',
+    img: [
+        fruitsColor['pineapple.png'].default,
+        fruitsLinear['pineapple.png'].default
+    ]
+}, {
+    name: 'Plum',
+    img: [
+        fruitsColor['plum.png'].default,
+        fruitsLinear['plum.png'].default
+    ]
+}, {
+    name: 'Pomegranate',
+    img: [
+        fruitsColor['pomegranate.png'].default,
+        fruitsLinear['pomegranate.png'].default
+    ]
+}, {
+    name: 'Star Fruit',
+    img: [
+        fruitsColor['star-fruit.png'].default,
+        fruitsLinear['star-fruit.png'].default
+    ]
+}, {
+    name: 'Strawberry',
+    img: [
+        fruitsColor['strawberry.png'].default,
+        fruitsLinear['strawberry.png'].default
+    ]
+}, {
+    name: 'Watermelon',
+    img: [
+        fruitsColor['watermelon.png'].default,
+        fruitsLinear['watermelon.png'].default
     ]
 }]
-
-// const fruits = [{
-//     name: 'Apple',
-//     img: [
-//         appleColor, 
-//         appleLinear
-//     ]
-// }, {
-//     name: 'Banana',
-//     img: [
-//         bananaColor, 
-//         bananaLinear
-//     ]
-// }, {
-//     name: 'Blackberry',
-//     img: [
-//         blackberryColor, 
-//         blackberryLinear
-//     ]
-// }, {
-//     name: 'Blueberries',
-//     img: [
-//         blueberriesColor, 
-//         blueberriesLinear
-//     ]
-// }, {
-//     name: 'Cherries',
-//     img: [
-//         cherriesColor,
-//         cherriesLinear
-//     ]
-// }, {
-//     name: 'Coconut',
-//     img: [
-//         coconutColor,
-//         coconutLinear
-//     ]
-// }, {
-//     name: 'Dragon Fruit',
-//     img: [
-//         dragonFruitColor,
-//         dragonFruitLinear
-//     ]
-// }, {
-//     name: 'Fig',
-//     img: [
-//         figColor,
-//         figLinear
-//     ]
-// }, {
-//     name: 'Grapes',
-//     img: [
-//         grapesColor,
-//         grapesLinear
-//     ]
-// }, {
-//     name: 'Kiwi',
-//     img: [
-//         kiwiColor,
-//         kiwiLinear
-//     ]
-// }, {
-//     name: 'Lemon',
-//     img: [
-//         lemonColor,
-//         lemonLinear
-//     ]
-// }, {
-//     name: 'Lychee',
-//     img: [
-//         lycheeColor,
-//         lycheeLinear
-//     ]
-// }, {
-//     name: 'Mango',
-//     img: [
-//         mangoColor,
-//         mangoLinear
-//     ]
-// }, {
-//     name: 'Melon',
-//     img: [
-//         melonColor,
-//         melonLinear
-//     ]
-// }, {
-//     name: 'Orange',
-//     img: [
-//         orangeColor,
-//         orangeLinear
-//     ]
-// }, {
-//     name: 'Papaya',
-//     img: [
-//         papayaColor,
-//         papayaLinear
-//     ]
-// }, {
-//     name: 'Peach',
-//     img: [
-//         peachColor,
-//         peachLinear
-//     ]
-// }, {
-//     name: 'Pear',
-//     img: [
-//         pearColor,
-//         pearLinear
-//     ]
-// }, {
-//     name: 'Pineapple',
-//     img: [
-//         pineappleColor,
-//         pineappleLinear
-//     ]
-// }, {
-//     name: 'Plum',
-//     img: [
-//         plumColor,
-//         plumLinear
-//     ]
-// }, {
-//     name: 'Pomegranate',
-//     img: [
-//         pomegranateColor,
-//         pomegranateLinear
-//     ]
-// }, {
-//     name: 'Star Fruit',
-//     img: [
-//         starFruitColor,
-//         starFruitLinear
-//     ]
-// }, {
-//     name: 'Strawberry',
-//     img: [
-//         strawberryColor,
-//         strawberryLinear
-//     ]
-// }, {
-//     name: 'Watermelon',
-//     img: [
-//         watermelonColor,
-//         watermelonLinear
-//     ]
-// }]
 
 export { fruits as default }
