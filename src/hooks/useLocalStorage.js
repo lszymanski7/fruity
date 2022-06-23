e4644c15b1d5b4d2467d6b26f8ae1ae9d152f5af
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-06-23 23:00:24 +0200

    Update useLocalStorage.js file - add functions to get and set data

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,7 +1,37 @@
-import fruitsDictionary from '../dictionary/fruits'
+import { useState } from 'react'
 
-const useLocalStorage = (key, value) => {
-    console.log('useLocalStorage')
+const useLocalStorage = (key, initialValue) => {
+
+    // Fetch data from localStorage
+    const getData = () => {
+        if (typeof window === 'undefined') {
+            return initialValue
+        }
+
+		try {
+            const data = window.localStorage.getItem(key)
+			return data ? JSON.parse(data) : initialValue
+		} catch (e) {
+			console.log(`Error getting localStorage key ('${key}'):`, e)
+            return initialValue
+        }
+    }
+
+    const [storedData, setStoredData] = useState(getData)
+
+    // Save data to localStorage
+    const setData = (value) => {
+        try {
+            setStoredData(value)
+            if (typeof window !== 'undefined') {
+                window.localStorage.setItem(key, JSON.stringify(value))
+            }
+        } catch (e) {
+            console.log(`Error setting localStorage key ('${key}'):`, e)
+        }
+    }
+
+    return [storedData, setData]
 }
 
 export { useLocalStorage as default }
