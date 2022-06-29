415ce432a10b6be0c20c1a98eaedb1eb2a098f7c
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-06-29 14:40:11 +0200

    Add Footer.js file - includes logo and icons attribution

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+
+    return (
+        <div className='footer'>
+            <p>
+                Logo created by <a href='https://www.flaticon.com/authors/freepik'>Freepik</a> | 
+                Icons created by <a href='https://www.flaticon.com/authors/smashicons'>Smashicons</a>
+            </p>
+        </div>
+    )
+}
+
+export { Footer as default }
