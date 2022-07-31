eaf3d8735fa9a40d8028e59e6b219bd64790ff3d
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-07-31 20:24:58 +0200

    Add uuid.js mock file

--- config/jest.config.js
+++ config/jest.config.js
@@ -3,9 +3,6 @@ module.exports = {
 	setupFilesAfterEnv: ['<rootDir>/src/__tests__/setupTests.js'],
 	testEnvironment: 'jest-environment-jsdom',
 	testRegex: '/__tests__/.*\\.(spec|test)\\.(js|jsx)$',
-	moduleNameMapper: {
-		'^uuid$': require.resolve('uuid')
-	},
 	transform: {
 		'^.+\\.(js|jsx)$': 'babel-jest',
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__tests__/fileTransformer.js'
