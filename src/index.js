c63a02fc2c323afbcb6cfa89393b6a5955ab67c4
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-09-17 22:28:44 +0200

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import Fruity from './components/Fruity'
-import store from './store'
+import App from './components/App'
 import '@csstools/normalize.css'
 import './assets/sass/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(
-    <Provider store={store}>
-        <Fruity />
-    </Provider>
-)
+root.render(<App />)
