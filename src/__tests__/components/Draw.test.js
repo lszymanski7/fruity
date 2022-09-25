af7b5ae1530aa841cce02896605c5c65227c7dd5
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-09-25 13:45:29 +0200

    Update Draw.test.js file

--- src/__tests__/components/Draw.test.js
+++ src/__tests__/components/Draw.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import Button from '../../components/Button'
+import Draw from '../../components/Draw'
 
 const props = {
     disabled: true,
@@ -10,26 +10,26 @@ const props = {
 
 describe('Button', () => {
     it('should be rendered correctly.', () => {
-        const { container } = render(<Button {...props} />)
+        const { container } = render(<Draw {...props} />)
         expect(container).toMatchSnapshot()
     })
 
-    it('should handle clicking.', async () => {
-        render(<Button {...props} disabled={false} />)
+    it('should handle clicking a button (Draw).', async () => {
+        render(<Draw {...props} disabled={false} />)
         const button = screen.getByRole('button')
         await userEvent.click(button)
         expect(props.handleDraw).toHaveBeenCalled()
     })
 
     it('should be disabled and have the correct text.', () => {
-        render(<Button {...props} />)
+        render(<Draw {...props} />)
         const button = screen.getByRole('button')
         expect(button).toBeDisabled()
         expect(button).toHaveTextContent('Select more fruits!')
     })
 
     it('should be enabled and have the correct text.', () => {
-        render(<Button {...props} disabled={false} />)
+        render(<Draw {...props} disabled={false} />)
         const button = screen.getByRole('button')
         expect(button).toBeEnabled()
         expect(button).toHaveTextContent('Draw a fruit!')
