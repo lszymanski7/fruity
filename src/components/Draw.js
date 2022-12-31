81a3f456ae9258aede68d41a953874f32ff057b4
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-12-31 03:30:54 +0100

    Update Draw.js, reset.svg and main.scss file

--- src/components/Draw.js
+++ src/components/Draw.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Draw = ({ disabled, handleDraw }) => {
+const Draw = ({ animation, disabled, handleDraw, handleReset }) => {
     return (
-        <div>
+        <div className="flexbox-row draw">
             <button
-                className="draw-button"
+                className="draw__button-draw"
                 disabled={disabled}
                 onClick={handleDraw}
                 type="button"
             >
-                {disabled ? 'Select more fruits!' : 'Draw a fruit!'}
+                {disabled ? 'SELECT MORE FRUITS' : 'DRAW A FRUIT'}
             </button>
+            <button
+                className={'draw__button-reset' + ' ' + animation}
+                disabled={disabled}
+                onClick={handleReset}
+                type="button"
+            />
         </div>
     )
 }
 
 Draw.propTypes = {
+    animation: PropTypes.string.isRequired,
     disabled: PropTypes.bool.isRequired,
-    handleDraw: PropTypes.func.isRequired
+    handleDraw: PropTypes.func.isRequired,
+    handleReset: PropTypes.func.isRequired
 }
 
 export { Draw as default }
