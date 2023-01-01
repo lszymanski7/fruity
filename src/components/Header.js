dfcf238a4d8dc7e8a4df48dad49715b6b25b167b
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2023-01-01 23:40:04 +0100

    Update Header.js and _header.scss files - change class names

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,19 @@ import ToggleSwitch from './ToggleSwitch'
 import { logos } from '../data/constants'
 
 const Header = () => {
-    // Application title
-    const title = 'FRUITY'
+    // Application name
+    const name = 'FRUITY'
 
     // Toggle switch states for dark and light mode
     const [isChecked, setIsChecked] = useState(false)
 
     return (
         <header className="flexbox-column header">
-            <div className="flexbox-row header__container-one">
+            <div className="flexbox-row header__widgets">
                 <a href="https://github.com/lszymanski7/fruity-app">
                     <img
                         alt="GitHub Logo"
-                        className="header__github"
+                        className="header__github-logo"
                         src={logos.github}
                     />
                 </a>
@@ -26,13 +26,13 @@ const Header = () => {
                     isChecked={isChecked}
                 />
             </div>
-            <div className="flexbox-row header__container-two">
+            <div className="flexbox-row header__brand">
                 <img
                     alt="Fruity Logo"
-                    className="header__logo"
+                    className="header__app-logo"
                     src={logos.fruity.size_256x256}
                 />
-                <h1 className="header__title">{title}</h1>
+                <h1 className="header__app-name">{name}</h1>
             </div>
         </header>
     )
